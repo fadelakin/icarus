2a0f4a759beeaa3a3074ab6638820ea2e6b18cf0
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-03-19 16:45:12 -0400

    preliminary

--- app/src/main/java/com/fisheradelakin/icarus/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/icarus/MainActivity.java
@@ -1,39 +1,72 @@
 package com.fisheradelakin.icarus;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import info.hoang8f.widget.FButton;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.startButton) FButton startButton;
+    @InjectView(R.id.stopButton) FButton stopButton;
+
+    SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.inject(this);
 
+        // shared preferences
+        mSharedPreferences = getSharedPreferences("settings", 0);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // enable/disable buttons
+        if(mSharedPreferences.getBoolean("isRunning", false)) {
+            disableButton(startButton);
+        } else {
+            disableButton(stopButton);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    // onclick implementation for start button
+    @OnClick(R.id.startButton)
+    public void start(View view) {
+        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+        mSharedPreferences.edit().putBoolean("isRunning", true).apply();
+        disableButton(startButton);
+        enableButton(stopButton);
+    }
+
+    // onclick implementation for stop button
+    @OnClick(R.id.stopButton)
+    public void stop(View view) {
+        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        mSharedPreferences.edit().putBoolean("isRunning", false).apply();
+        disableButton(stopButton);
+        enableButton(startButton);
+    }
+
+    private void disableButton(FButton button) {
+        button.setEnabled(false);
+        button.setButtonColor(getResources().getColor(R.color.gray_disabled));
+        button.setShadowColor(getResources().getColor(R.color.gray_disabled_shadow));
+        button.setTextColor(getResources().getColor(R.color.text_button_disabled));
+    }
+
+    private void enableButton(FButton button) {
+        button.setEnabled(true);
+        button.setButtonColor(getResources().getColor(R.color.blue_enabled));
+        button.setShadowColor(getResources().getColor(R.color.blue_enabled_shadow));
+        button.setTextColor(getResources().getColor(R.color.text_button_enabled));
+    }
+
+
 }
